03f9ce80b1a3a7fc9ca9f9c1d6e82d62edf413d3
BenardBett <kiplangatbett5@gmail.com>
2020-10-02 16:35:36 +0300

    created a js file

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var daysOfWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames=["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"];  
