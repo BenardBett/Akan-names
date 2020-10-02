7dee8fb3b617ef0c03325b30901184ebfd919acf
BenardBett <kiplangatbett5@gmail.com>
2020-10-02 16:48:09 +0300

    added a function in js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-var daysOfWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"];
+var daysOfWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames=["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"];  
+
+function userInput(){
+    var century=parseInt(document.getElementById("century").value);
+    var year=parseInt(document.getElementById("year").value);
+    var month =parseInt(document.getElementById("month").value);
+    var day=parseInt(document.getElementById("dateOfBirth").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+}
