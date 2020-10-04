7093c84c3cf770c7907b3d98e9cd05612082029d
BenardBett <kiplangatbett5@gmail.com>
2020-10-04 18:11:05 +0300

    modified script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+ //arrays declaration
  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
